bf7f241a2c5008044f1a19bcfad4032f2a3ef47d
misha <mishazzz136@gmail.com>
2026-05-27 20:54:58 +0500

    Добавьте файлы проекта.

---
+++ RecipeManager/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+using namespace std;
+// Класс для хранения данных о блюде 
+
+class Recipe {
+public:
+    string name;
+    double calories;
+    Recipe(string n, double c) : name(n), calories(c) {}
+};
+
+//конец
+
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    vector<Recipe> myRecipes;
+    string name;
+    double cal;
+
+    cout << "--- Калькулятор Калорий ---" << endl;
+    cout << "\nВведите название блюда (или напишите 'выход' для завершения): ";
+    while (true) {
+        cin >> name;
+        if (name == "выход") break; // Выход из цикла
+
+        cout << "Введите калорийность: ";
+        cin >> cal;
+        myRecipes.push_back(Recipe(name, cal));
+        cout << "\nДобавлено! Введите следующее блюдо или 'выход': ";
+    }
+
+
+    //конец цикла 
+
+
+    ofstream outFile("recipes.txt");
+    for (const auto& r : myRecipes) {
+        outFile << r.name << " " << r.calories << endl;
+    }
+    outFile.close();
+    cout << "\nРецепты сохранены в файл recipes.txt!" << endl;
+
+    cout << "\n--- Итоговый список ---" << endl;
+    for (const auto& r : myRecipes) {
+        cout << r.name << " : " << r.calories << " ккал" << endl;
+    }
+
+    return 0;
+}
